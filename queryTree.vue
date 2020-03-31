40577c40da6fd4e00124a458e32b34cb9a078525
Canfengchen <76937494@qq.com>
2020-03-31 21:58:25 +0800

    Create queryTree.vue
    O(n)方式条件筛选结构树

---
+++ queryTree.vue
@@ -0,0 +1,26 @@
+function queryTree(tree, id) {
+  let stark = []
+  stark = start.concat(tree)
+  while(stark.length > 0){
+    let tmp = stark.shift()
+    if (tmp.children) {
+      stark.concat(tmp.children)
+    }
+    if(tmp.id === id){
+      return tmp.value
+    }
+  }
+  return ''
+}
+
+tree结构
+{
+  id: '1',
+  value: '1',
+  children: [
+    {
+      id: '2',
+      value: '2'
+    }
+  ]
+}
